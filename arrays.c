173e2beec710739f32d595937fe5ea5bc16ac10b
Bankole Esan <techybanky@gmail.com>
2019-10-15 22:37:09 +0100

    practice

--- arrays.c
+++ arrays.c
@@ -1,8 +1,18 @@
 /* Practice File for several classes */
 #include<stdio.h>
 
+// functions should be declared before main()
+void guessNumber(int guess){
+    if (guess == 555){
+        printf("Correct. You guessed it!\n");
+    } else if (guess < 555){
+    	printf("Your guess is too low.\n");
+    } else {
+    	printf("Your guess is too high.\n");
+    }
+};
 
-int main(){ 
+int main(void){ 
   
   // Display Hello World
   printf("Hellow World\n\n");
@@ -82,6 +92,17 @@ int main(){
 
 			average /= 5.0;
 			printf("The average marks obtained in subject %d is: %.2f\n", i, average);
+
+       
 		}
 
+    guessNumber(500);
+    guessNumber(600);
+    guessNumber(555);
+
+    
 }
+  // CONDITIONALS
+  // COMPARATORS => ==, >, >=, <, <=, &&, ||, !=. 
+    
+
